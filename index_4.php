7c0372c3b30d0ac6cc0c66f7a6097eaf9db87da6
janvhz <johnnvhellebasbas@gmail.com>
2024-09-19 22:49:54 +0800

    Add files via upload

---
+++ index_4.php
@@ -0,0 +1,44 @@
+<?php
+$dataFile = 'group7_data.txt';
+if (file_exists($dataFile)) {
+    $teamArray = file($dataFile, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+} else {
+
+    $teamData = 
+        "Rechelle|image1.png|Group Member|candidorechelleanne@gmail.com|I'm Rechelle Anne Candido, a third-year BSIT student. A working student, nail tech, makeup artist and I own a small business. I work hard because I want to pursue my course and become successful in the future.\n" .
+        "Johnn Vhelle|Image2.jpg|Group Member|johnnvhellebasbas@gmail.com|Hi there! I'm Vhelle, and I'm a third-year college student at PLMun. I currently take the BSIT program. Watching movies and reading stories are two of my interests, and I'd like to master digital art someday.\n" .
+        "Ian Christoper|Image5.jpeg|Group Leader|icasenci12@gmail.com|I'm Ian, a third-year BSIT student at PLMun! Just a guy trying to do better every passing day and hoping that life takes me to a simple yet better place.\n" .
+        "Justin|Image4.jpg|Group Member|Justinpadora@gmail.com|I'm Justin Padora, a third-year BSIT student who also works as a crew leader at a fast food restaurant. I'm motivated to work hard in order to achieve my goal of graduating.\n";
+    file_put_contents($dataFile, $teamData);
+    $teamArray = file($dataFile, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Group 7 Team Portfolio</title>
+    <link rel="stylesheet" href="style.css">
+    <button class="manage" onclick="window.location.href='submit.php'">Manage Team</button>
+</head>
+<body>
+    <div class="group-name">BSIT 3L - Group 7</div>
+    <p class="group-description">"Integrative Programming"</p>
+    <div class="team-members">
+        <?php foreach ($teamArray as $index => $memberData): 
+            $member = explode('|', $memberData); ?>
+        <div class="team-member">
+            <img id="imagePreview<?php echo $index + 1; ?>" src="<?php echo $member[1]; ?>" alt="Team Member <?php echo $index + 1; ?>">
+            <h2><?php echo htmlspecialchars($member[0]); ?></h2>
+            <div class="role-label">Role</div>
+            <p class="role"><?php echo htmlspecialchars($member[2]); ?></p>
+            <div class="contact-label">Contact</div>
+            <p class="contact"><?php echo htmlspecialchars($member[3]); ?></p>
+            <div class="bio-label">Bio</div>
+            <p class="bio"><?php echo htmlspecialchars($member[4]); ?></p>
+        </div>
+        <?php endforeach; ?>
+    </div>
+</body>
+</html>
